46f73ac0b5ebfb2dc68b36f4e3167e1d8292a82e
Merieno <admin@Merienos-MacBook-Pro.local>
2026-05-26 13:33:29 +0100

    Initial commit - Frank Gadgets store

---
+++ config/db.example.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'your_db_username');
+define('DB_PASS', 'your_db_password');
+define('DB_NAME', 'frank_gadgets');
+// ... copy this file to db.php and fill in your details
